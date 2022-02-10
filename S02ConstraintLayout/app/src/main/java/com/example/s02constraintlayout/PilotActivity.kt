5af6aa686dea85127ee1f02022a650e2ff71a44b
1847622 <1847622@cstj.qc.ca>
2022-02-10 17:04:06 -0500

    10 février il manque juste l'animation

--- S02ConstraintLayout/app/src/main/java/com/example/s02constraintlayout/PilotActivity.kt
+++ S02ConstraintLayout/app/src/main/java/com/example/s02constraintlayout/PilotActivity.kt
@@ -2,10 +2,52 @@ package com.example.s02constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.s02constraintlayout.databinding.ActivityPilotBinding
+import com.example.s02constraintlayout.domain.models.Pilot
 
 class PilotActivity : AppCompatActivity() {
+
+    //Permettre d'accéder aux composant graphiques (Button , txtView , ... ) de l'interface
+    private lateinit var binding: ActivityPilotBinding
+
+    private val _pilot = Pilot("Bee Zoon", 10)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pilot)
+        //Transforme et charge le xml en mémoire dans le binding
+        binding = ActivityPilotBinding.inflate(layoutInflater)
+        // Créer l'interface à patir du binding
+        setContentView(binding.root)
+
+        refreshUI()
+
+
+        binding.btnStart.setOnClickListener() {
+            if (_pilot.canFly()) {
+                _pilot.fly(binding.sldRevolution.value.toInt(), binding.swtTraining.isChecked)
+
+                refreshUI()
+            } else {
+                //TODO : Snackbar chez Raymond
+            }
+
+        }
+
+
     }
+
+    private fun refreshUI(){
+        with(binding) {
+            txvPilotName.text = _pilot.name
+            txvLevel.text = _pilot.level.toString()
+            txvLife.text = _pilot.life.toString()
+            txvShield.text = _pilot.shield.toString()
+            txvEnergy.text = _pilot.energy.toString()
+            txvCube.text = _pilot.cube.toString()
+
+        }
+    }
+
+
 }
