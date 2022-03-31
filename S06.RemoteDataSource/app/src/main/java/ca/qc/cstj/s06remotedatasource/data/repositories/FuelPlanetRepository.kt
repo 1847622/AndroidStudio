91af9c23767a5bca3adca968dce33cd673b3cf5a
1847622 <1847622@cstj.qc.ca>
2022-03-31 15:45:04 -0400

    s0006

--- S06.RemoteDataSource/app/src/main/java/ca/qc/cstj/s06remotedatasource/data/repositories/FuelPlanetRepository.kt
+++ S06.RemoteDataSource/app/src/main/java/ca/qc/cstj/s06remotedatasource/data/repositories/FuelPlanetRepository.kt
@@ -1,8 +1,13 @@
 package ca.qc.cstj.s06remotedatasource.data.repositories
 
+import ca.qc.cstj.s06remotedatasource.core.Contants
+import ca.qc.cstj.s06remotedatasource.core.LoadingResource
 import ca.qc.cstj.s06remotedatasource.core.Resource
 import ca.qc.cstj.s06remotedatasource.data.datasources.FuelPlanetDataSource
 import ca.qc.cstj.s06remotedatasource.domain.models.Planet
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 // axios = fuel
 
@@ -10,6 +15,7 @@ class FuelPlanetRepository {
 
     private val planetDataSource = FuelPlanetDataSource()
 
+    // Retrouve les planètes sans mise a jour
     suspend fun retrieveAll(): Resource<List<Planet>> {
         return try {
              Resource.Success(planetDataSource.retrieveAll())
@@ -17,5 +23,36 @@ class FuelPlanetRepository {
              Resource.Error(ex,"Erreur Serveur")
         }
     }
+
+    // Retrouver les planètes avec une mise à jour régulière
+    suspend fun retrieveAllWithUpdate() : Flow<Resource<List<Planet>>> {
+        return flow {
+           while (true){
+               try {
+                   emit(Resource.Success(planetDataSource.retrieveAll()))
+               }catch (ex: Throwable){
+                   emit(Resource.Error(ex,"Erreur Serveur"))
+               }
+               delay(Contants.RefreshRates.PLANETS_REFRESH_RATE)
+           }
+        }
+
+    }
+
+    suspend fun retrieveAllWithLoading(): Flow<LoadingResource<List<Planet>>> {
+        return flow {
+            while (true){
+                try {
+                    emit(LoadingResource.Loading())
+                    delay(1500)
+                    emit(LoadingResource.Success(planetDataSource.retrieveAll()))
+                }catch (ex: Throwable){
+                    emit(LoadingResource.Error(ex,"Erreur Serveur"))
+                }
+                delay(Contants.RefreshRates.PLANETS_REFRESH_RATE)
+            }
+        }
+    }
+
 
 }
--- S06.RemoteDataSource/app/src/main/java/ca/qc/cstj/s06remotedatasource/presentation/ui/planet/PlanetListActivity.kt
+++ S06.RemoteDataSource/app/src/main/java/ca/qc/cstj/s06remotedatasource/presentation/ui/planet/PlanetListActivity.kt
@@ -2,9 +2,11 @@ package ca.qc.cstj.s06remotedatasource.presentation.ui.planet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import ca.qc.cstj.s06remotedatasource.core.LoadingResource
 import ca.qc.cstj.s06remotedatasource.core.Resource
 import ca.qc.cstj.s06remotedatasource.core.notifyAllItemChanged
 import ca.qc.cstj.s06remotedatasource.databinding.ActivityPlanetListBinding
@@ -31,7 +33,7 @@ class PlanetListActivity : AppCompatActivity() {
             adapter = planetRecyclerViewAdapter
 
         }
-        viewModel.planets.observe(this){
+       /* viewModel.planets.observe(this){
             when(it){
                 is Resource.Error -> {
                     Toast.makeText(this,it.message, Toast.LENGTH_LONG).show()
@@ -44,7 +46,24 @@ class PlanetListActivity : AppCompatActivity() {
             }
 
 
-        }
+        }*/
 
+        viewModel.planetsLoading.observe(this){
+            when(it){
+                is LoadingResource.Error -> {
+                    Toast.makeText(this,it.message, Toast.LENGTH_LONG).show()
+                }
+                is LoadingResource.Loading -> {
+                    binding.rcvPlanets.visibility = View.INVISIBLE
+                    binding.lpbLoading.show()
+                }
+                is LoadingResource.Success -> {
+                        binding.rcvPlanets.visibility = View.VISIBLE
+                        binding.lpbLoading.hide()
+                        planetRecyclerViewAdapter.planets = it.data!!
+                        planetRecyclerViewAdapter.notifyAllItemChanged()
+                    }
+                }
+            }
     }
 }
